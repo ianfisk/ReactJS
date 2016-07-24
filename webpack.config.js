629b8b9e64dd5edcfa0a9dbab54b8367eb635625
Ian Fisk <ian.fisk@faithlife.com>
2016-07-24 11:12:27 -0700

    Init commit. Add package and webpack configurations.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// get the html-webpack-plugin module and save it to an object
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+})
+
+module.exports = {
+	entry: [
+		'./app/index.js'
+	],
+	output: {
+		path: __dirname + '/dist',
+		filename: "index_bundle.js"
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+		]
+	},
+	plugins: [HtmlWebpackPluginConfig]
+}
