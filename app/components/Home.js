72e5017edbff2ad2560ca890a5260e282f49506e
Ian Fisk <i.fisk32@gmail.com>
2016-08-29 23:19:58 -0700

    Change motto

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ var Home = React.createClass({
 		return (
 			<div className='jumbotron col-sm-12 text-center' style={transparentBackground}>
 				<h1>Github Battle</h1>
-				<p className='lead'>Some fancy motto</p>
+				<p className='lead'>One battle to rule them all</p>
 				<Link to='/playerOne'>
 					<button type='button' className='btn btn-lg btn-success'>Get Started</button>
 				</Link>
